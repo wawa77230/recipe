7797920506b81f4c2837416e6aee9114d4e0308f
Warren Neves <warrenneves@hotmail.fr>
2020-06-11 03:12:22 +0200

    Ajout fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Recette;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1;$i<25;$i++)
+        {
+            $recette = new Recette();
+
+            $recette->setTitre("Recette".$i)
+                    ->setSousTitre("Lorem ipsum dolor sit amet")
+                    ->setIngredients(["Apple, Orange, Plum"]);
+
+            $manager->persist($recette);
+        }
 
         $manager->flush();
     }
